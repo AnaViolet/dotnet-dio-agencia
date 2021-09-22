dbf3215f5439700902347721acd0aee33e4b99f1
AnaViolet <prabelo79@gmail.com>
2021-09-22 20:58:53 -0300

    Classes

---
+++ AgenciaBancaria/AgenciaBancaria.App/Program.cs
@@ -0,0 +1,33 @@
+﻿using AgenciaBancaria.Dominio;
+using System;
+
+namespace AgenciaBancaria.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Endereco endereco = new Endereco("Rua teste", "0000-00", "Pederneiras", "SP");
+                Cliente cliente = new Cliente("Carla", "23456789", "0000000", endereco);
+
+                ContaCorrente conta = new ContaCorrente(cliente, 100);
+
+                Console.WriteLine("Conta " + conta.Situacao + ": " + conta.NumeroConta + "-" + conta.DigitoVerificador);
+                
+                string senha = "abc123de";
+                conta.Abrir(senha);
+                
+                Console.WriteLine("Conta " + conta.Situacao + ": " + conta.NumeroConta + "-" + conta.DigitoVerificador);
+
+                conta.Sacar(10, senha);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+    }
+}
---
+++ AgenciaBancaria/AgenciaBancaria.Dominio/Cliente.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgenciaBancaria.Dominio
+{
+    public class Cliente
+    {
+        public Cliente(String nome, string cpf, string rg, Endereco endereco)
+        {
+            //Aqui garantimos que nosso objeto será válido
+            //ValidaStringVazia chama o método como se fosse da classe String
+            Nome = nome.ValidaStringVazia();
+            CPF = cpf.ValidaStringVazia();
+            RG = rg.ValidaStringVazia();
+
+            // Null-coalescing operator (??): retorna endereco se não for null,
+            // mas se for kança a exceção.
+            Endereco = endereco ?? throw new Exception("Endereço deve ser informado.");
+        }
+
+        public string Nome { get; private set; }
+        public string CPF { get; private set; }
+        public string RG { get; private set; }
+        public Endereco Endereco { get; private set; }
+    }
+}
